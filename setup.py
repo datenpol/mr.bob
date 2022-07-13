abfe2c910f547cf5815018d44b518e63b1383d3f
Ahmed Aly <ahmed.aly@datenpol.at>
2022-07-13 09:33:53 +0200

    mr.bob release 0.1.4

--- setup.py
+++ setup.py
@@ -1,81 +1,40 @@
-# -*- coding: utf-8 -*-
+import setuptools
 
-import os
-import sys
-import codecs
-
-from setuptools import setup
-from setuptools import find_packages
-
-install_requires = [
-    'setuptools',
-    'six>=1.16.0',
+long_description = open("README.md").read()
+required = [
+    "six",
+    "ordereddict",
+    "importlib",
+    "Jinja2",
+    "argparse"
 ]
 
-if (3,) < sys.version_info < (3, 3):
-    # Jinja 2.7 drops Python 3.2 compat.
-    install_requires.append('Jinja2>=2.5.0,<2.7dev')
-else:
-    install_requires.append('Jinja2>=3.1.2')
-
-try:
-    import importlib  # NOQA
-except ImportError:
-    install_requires.append('importlib')
-
-try:
-    from collections import OrderedDict  # NOQA
-except ImportError:
-    install_requires.append('ordereddict')
-
-try:
-    import argparse  # NOQA
-except ImportError:
-    install_requires.append('argparse')
-
-
-def read(*rnames):
-    return codecs.open(os.path.join(os.path.dirname(__file__), *rnames), 'r', 'utf-8').read()
-
-
-setup(name='mr.bob',
-      version='0.1.4',
-      description='Bob renders directory structure templates',
-      long_description=read('README.rst') + '\n' + read('HISTORY.rst'),
-      classifiers=[
-          "Programming Language :: Python",
-          "Programming Language :: Python :: Implementation :: CPython",
-          "Programming Language :: Python :: Implementation :: PyPy",
-          "Programming Language :: Python :: 2.6",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.2",
-          "Programming Language :: Python :: 3.3",
-          "Programming Language :: Python :: 3.4",
-          "Programming Language :: Python :: 3.10",
-      ],
-      author='Ahmed Aly, Domen Kozar, Tom Lazar',
-      author_email='',
-      url='https://github.com/datenpol/mr.bob.git',
-      license='BSD',
-      packages=find_packages(),
-      install_requires=install_requires,
-      extras_require={
-          'test': [
-              'nose',
-              'coverage<3.6dev',
-              'flake8>2.0',
-              'mock',
-          ],
-          'development': [
-              'zest.releaser',
-              'Sphinx',
-          ],
-      },
-      entry_points="""
+setuptools.setup(
+    name="mr.bob",
+    version="0.1.4",  # eg:1.0.0
+    author="Ahmed Aly, Domen Kozar, Tom Lazar",
+    author_email="ahmed.aly@datenpol.at",
+    license="BSD",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    description="Bob renders directory structure templates",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/datenpol/mr.bob.git",
+    key_words="temlates, render, mr.bob, odoo",
+    install_requires=required,
+    entry_points="""
       [console_scripts]
       mrbob = mrbob.cli:main
       """,
-      include_package_data=True,
-      zip_safe=False,
-      )
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
